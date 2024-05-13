79483e3a99c0287af9738f400e327beebe5211ec
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-05-13 18:08:08 +0100

    changePath

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -18,7 +18,7 @@ function AppRoutes() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="https://sabaembroidery.onrender.com/" element={<Home />} />
           <Route
             path="/best-selling"
             element={<Products title="best selling" categories />}
