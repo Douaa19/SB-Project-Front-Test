5676372ffc20c912e2a0a4c1a3b701383ca23a03
Douaa19 <77000956+Douaa19@users.noreply.github.com>
2024-06-26 23:26:32 +0100

    BackendURL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-// export const BACK_URL = "https://sabaembroidery-api.onrender.com";
-export const BACK_URL = "http://localhost:8008/api";
+export const BACK_URL = "https://sabaembroidery-api.onrender.com";
